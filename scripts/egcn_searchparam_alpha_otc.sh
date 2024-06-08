555c083be0c6c1076de5ae7d243b2766731ccbef
ykrmm <yanniskarmim@hotmail.fr>
2024-06-08 19:51:56 +0200

    clean scripts

--- scripts/egcn_searchparam_alpha_otc.sh
+++ scripts/egcn_searchparam_alpha_otc.sh
@@ -19,3 +19,25 @@ python tw_benchmark/run.py \
  task.engine.epoch=30 \
  task.engine.batch_size=1024 \
  optim.optimizer.weight_decay=0 \
+
+python tw_benchmark/run.py \
+ --multirun \
+ dataset=DGB-Bitcoin-OTC \
+ wandb_conf.name=EGCNH_SearchParam_OTC \
+ gpu=1 \
+ lr=0.0001,0.001,0.01,0.1 \
+ model=EvolveGCNH \
+ model.evolving=False \
+ model.pred_next=False \
+ model.clip_grad=False \
+ model.link_pred.num_layers_rnn=2 \
+ model.link_pred.time_length=3 \
+ model.link_pred.improved=True \
+ model.link_pred.cached=False \
+ model.link_pred.add_self_loops=False \
+ model.link_pred.use_edge_weight=False \
+ model.link_pred.undirected=True \
+ task.engine.n_runs=1 \
+ task.engine.epoch=30 \
+ task.engine.batch_size=1024 \
+ optim.optimizer.weight_decay=0 \
--- scripts/egcn_searchparam_vote.sh
+++ scripts/egcn_searchparam_vote.sh
@@ -1,20 +1,21 @@
 python tw_benchmark/run.py \
  --multirun \
- dataset=DGB-CanParl \
- wandb_conf.name=EGCNH_SearchTW_Can \
- gpu=1 \
- lr=0.01 \
+ dataset=DGB-UNvote \
+ wandb_conf.name=EGCNH_SearchParam_Vote \
+ gpu=2 \
+ lr=0.0001,0.001,0.01,0.1 \
  model=EvolveGCNH \
  model.evolving=False \
  model.pred_next=False \
- model.clip_grad=True \
+ model.clip_grad=False \
  model.link_pred.num_layers_rnn=2 \
- model.link_pred.time_length=1,2,3,4,5,6,7,8,9,10,-1 \
+ model.link_pred.time_length=3 \
  model.link_pred.improved=True \
  model.link_pred.cached=False \
  model.link_pred.add_self_loops=False \
  model.link_pred.use_edge_weight=False \
  model.link_pred.undirected=True \
  task.engine.n_runs=1 \
+ task.engine.epoch=30 \
  task.engine.batch_size=1024 \
- optim.optimizer.weight_decay=0.0001 \
+ optim.optimizer.weight_decay=0 \

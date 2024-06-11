52b647ed6b53eb906a8d03647f2f991ab8025c06
ykrmm <yanniskarmim@hotmail.fr>
2024-06-11 22:55:13 +0200

    add scripts tw egcn bitcoin

--- scripts/egcn_searchtw_alpha.sh
+++ scripts/egcn_searchtw_alpha.sh
@@ -1,18 +1,18 @@
 python tw_benchmark/run.py \
  --multirun \
  dataset=DGB-Bitcoin-Alpha \
- wandb_conf.name=EGCNH_SearchParam_Alpha \
+ wandb_conf.name=EGCNH_SearchTW_Alpha \
  gpu=1 \
- lr=0.0001,0.001,0.01,0.1 \
+ lr=0.00001 \
  model=EvolveGCNH \
  model.evolving=False \
  model.pred_next=False \
  model.clip_grad=False \
- model.link_pred.num_layers_rnn=1,2 \
- model.link_pred.time_length=1 \
- model.link_pred.improved=True,False \
- model.link_pred.cached=False,True \
- model.link_pred.add_self_loops=False \
+ model.link_pred.num_layers_rnn=1 \
+ model.link_pred.time_length=1,2,3,4,5,6,7,8,9,10,-1 \
+ model.link_pred.improved=True \
+ model.link_pred.cached=True \
+ model.link_pred.add_self_loops=True \
  model.link_pred.use_edge_weight=False \
  model.link_pred.undirected=True \
  task.engine.n_runs=1 \

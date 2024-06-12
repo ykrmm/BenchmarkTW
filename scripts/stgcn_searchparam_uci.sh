cb7e9d36fca09ad6b70f64b34b080bddbd8731eb
ykrmm <yanniskarmim@hotmail.fr>
2024-06-12 10:06:15 +0200

    discretize uci message, scripts search message

---
+++ scripts/stgcn_searchparam_uci.sh
@@ -0,0 +1,20 @@
+python tw_benchmark/run.py \
+ --multirun \
+ dataset=DGB-UCI-Message \
+ wandb_conf.name=STGCN_SearchParam_Message \
+ gpu=2 \
+ lr=0.1,0.01,0.001,0.0001,0.00001\
+ model=STGCN \
+ model.evolving=False \
+ model.pred_next=False \
+ model.clip_grad=False \
+ model.link_pred.window=1 \
+ model.link_pred.kernel_size=1 \
+ model.link_pred.K=2 \
+ model.link_pred.normalization=sym \
+ model.link_pred.undirected=True \
+ optim.optimizer.weight_decay=0 \
+ task.engine.epoch=50 \
+ task.engine.batch_size=1024 \
+ task.engine.n_runs=1 \
+ 
